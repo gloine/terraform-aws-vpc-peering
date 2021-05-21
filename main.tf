0c2e764b36dca599caac2fb8b2fe2fd27a1e6f9a
Jaehyung Lee <gloine@gmail.com>
2021-05-21 16:38:25 +0900

    removed aws version constraint

--- main.tf
+++ main.tf
@@ -36,91 +36,73 @@ locals {
 // Any new region must be peered to all existing region.
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "us-east-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "us-east-2"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "us-west-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "us-west-2"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "ap-east-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "ap-south-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "ap-northeast-2"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "ap-southeast-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "ap-southeast-2"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "ap-northeast-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "ca-central-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "eu-central-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "eu-west-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "eu-west-2"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "eu-west-3"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "eu-north-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "me-south-1"
 }
 
 provider "aws" {
-  version = "~> 2.0"
   alias = "sa-east-1"
 }
